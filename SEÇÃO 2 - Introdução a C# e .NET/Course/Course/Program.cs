e20ec160755c91aab3b196fd7b0e50dc0670f04b
Giovanna Antonieli <129458706+GiihAntonieli@users.noreply.github.com>
2024-04-18 00:50:03 -0300

    arquivo

---
+++ SEÇÃO 2 - Introdução a C# e .NET/Course/Course/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Course
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            sbyte x = 100;
+            Console.WriteLine(x);
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ SEÇÃO 2 - Introdução a C# e .NET/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+namespace PrimeiroProjeto
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!!");
+        }
+    }
+}
+
+
+
